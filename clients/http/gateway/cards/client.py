283528e894ba9ddb0e129b5e2f7bf2f0aa0bf7aa
spartanezka <kuznezov188@yandex.ru>
2026-03-12 15:56:57 +0300

    Знакомство с HTTP API клиентами.Практика: Написание HTTP API клиента

--- clients/http/gateway/cards/client.py
+++ clients/http/gateway/cards/client.py
@@ -5,8 +5,14 @@ from httpx import Response
 from clients.http.client import HTTPClient
 
 
-class IssueCardRequestDict(TypedDict):
-    """Структура данных для создания виртуальной/физической карты."""
+class IssueVirtualCardRequestDict(TypedDict):
+    """Структура данных для создания виртуальной карты."""
+
+    userId: str
+    accountId: str
+
+class IssuePhysicalCardRequestDict(TypedDict):
+    """Структура данных для создания физической карты."""
 
     userId: str
     accountId: str
@@ -15,7 +21,7 @@ class IssueCardRequestDict(TypedDict):
 class CardsGatewayHTTPClient(HTTPClient):
     """Клиент для взаимодействия с /api/v1/cards сервиса http-gateway."""
 
-    def issue_virtual_card_api(self, request: IssueCardRequestDict) -> Response:
+    def issue_virtual_card_api(self, request: IssueVirtualCardRequestDict) -> Response:
         """Создание виртуальной карты.
 
         :param request: Словарь с данными для создания виртуальной карты.
@@ -23,7 +29,7 @@ class CardsGatewayHTTPClient(HTTPClient):
         """
         return self.post("/api/v1/cards/issue-virtual-card", json=request)
 
-    def issue_physical_card_api(self, request: IssueCardRequestDict) -> Response:
+    def issue_physical_card_api(self, request: IssuePhysicalCardRequestDict) -> Response:
         """Создание физической карты.
 
         :param request: Словарь с данными для создания физической карты.
